fec8a9aa9b34fa212ffacd40c1ede8ec2f2597fb
Emiliojcm <121568964+Emiliojcm@users.noreply.github.com>
2023-10-17 15:42:55 -0300

    implementando a tela de login parte um

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HomeComponent } from './modules/home/home.component';
+
+//PrimeNG
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { ToastModule } from 'primeng/toast';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -17,7 +26,12 @@ import { ReactiveFormsModule } from '@angular/forms';
     BrowserAnimationsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    
+    //PrimeNG
+    CardModule,
+    InputTextModule,
+    ButtonModule,
+    ToastModule
+
 
   ],
   providers: [],
